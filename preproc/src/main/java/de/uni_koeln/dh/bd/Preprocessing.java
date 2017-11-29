a293737a808841ffbbe6806be2d174af1c7a966f
Stefan Krause <s.krause@uni-koeln.de>
2017-11-29 19:36:02 +0100

    Split into preprocessing-project and webapp-project

---
+++ preproc/src/main/java/de/uni_koeln/dh/bd/Preprocessing.java
@@ -0,0 +1,9 @@
+package de.uni_koeln.dh.bd;
+
+public class Preprocessing {
+	
+	public static void main(String[] args) {
+	
+	}
+	
+}
---
+++ preproc/src/test/java/de/uni_koeln/dh/bd/PreprocessingTest.java
@@ -0,0 +1,38 @@
+package de.uni_koeln.dh.bd;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class PreprocessingTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public PreprocessingTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( PreprocessingTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
