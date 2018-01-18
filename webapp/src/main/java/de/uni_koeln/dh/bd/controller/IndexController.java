2ee0a02a350eb19870f248b0769ca462cd661316
Stefan Krause <s.krause@uni-koeln.de>
2018-01-18 17:08:11 +0100

    Changed name, merged map and places

--- webapp/src/main/java/de/uni_koeln/dh/bd/controller/IndexController.java
+++ webapp/src/main/java/de/uni_koeln/dh/bd/controller/IndexController.java
@@ -1,13 +1,8 @@
 package de.uni_koeln.dh.bd.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import de.uni_koeln.dh.bd.model.place.Place;
 
 @Controller
 public class IndexController {
@@ -21,24 +16,6 @@ public class IndexController {
 	@RequestMapping(value = "/about")
 	public String about() {
 		return "about";
-	}
-
-	// TODO map
-	@RequestMapping(value = "/map")
-	public String map(Model model) {
-		List<Place> places = new ArrayList<Place>();
-		Place london = new Place(51.508, -0.11);
-		london.addPlaceName("London");
-		london.addPlaceName("London City");
-		london.addTexts("London is a big City in the North!");
-		london.addTexts("London is a bigger than the most other cities!");
-		places.add(london);
-		Place colonia = new Place(50.945312, 6.945928);
-		colonia.addPlaceName("Köln");
-		colonia.addTexts("Very popular city. especially in february!");
-		places.add(colonia);
-		model.addAttribute("places", places);
-		return "map";
 	}
 
 }
--- webapp/src/main/java/de/uni_koeln/dh/bd/controller/ProcessingController.java
+++ webapp/src/main/java/de/uni_koeln/dh/bd/controller/ProcessingController.java
@@ -1,8 +1,13 @@
 package de.uni_koeln.dh.bd.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import de.uni_koeln.dh.bd.model.place.Place;
 
 // TODO name?
 @Controller
@@ -17,7 +22,19 @@ public class ProcessingController {
 	// TODO places
 	@RequestMapping(value = "/places")
 	public String places(Model model) {
-	    return "places";
+		List<Place> places = new ArrayList<Place>();
+		Place london = new Place(51.508, -0.11);
+		london.addPlaceName("London");
+		london.addPlaceName("London City");
+		london.addTexts("London is a big City in the North!");
+		london.addTexts("London is a bigger than the most other cities!");
+		places.add(london);
+		Place colonia = new Place(50.945312, 6.945928);
+		colonia.addPlaceName("Köln");
+		colonia.addTexts("Very popular city. especially in february!");
+		places.add(colonia);
+		model.addAttribute("places", places);
+		return "places";
 	}
 	
 }
