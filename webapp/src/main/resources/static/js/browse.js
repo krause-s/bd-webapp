5a699e292eaeb7584f3fce622d6ec28ec100db46
Stefan Krause <skrause.koeln@gmail.com>
2018-03-07 22:21:55 +0100

    Responsive table layout (final), file upload function

--- webapp/src/main/resources/static/js/browse.js
+++ webapp/src/main/resources/static/js/browse.js
@@ -1,19 +1,44 @@
 $(document).ready(function($) {
-	$('#testTable').DataTable({
+	init();
+});
+
+function init() {
+	var table = "t";
+//	var processing = "r";
+	var length = "l";
+	var info = "i";
+	var pagination = "p";
+	
+	$('#songs').DataTable({
 //		"processing": true,
 		
 		"dom": 
-//			"<'panel panel-default'<'panel-body'tr>" +
-			"<'panel panel-default'<'panel-body't>" +
-			"<'panel-footer'<'row'" +
-			"	<'col-sm-4'l>" +
-			"	<'col-sm-4'i>" +
-			"	<'col-sm-4'p>>>>",
-		
-		"scrollX": true,	
-		"lengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
-		"order" : [ [ 0, 'asc' ], [ 1, 'asc' ], [ 2, 'asc' ], [ 3, 'asc' ] ],
+			getTag("panel-body", table /*+ processing*/) +			
+			getTag("panel-footer",
+				getTag("#paginationRow.row",
+						getTag("col-lg-5 col-sm-4 hidden-xs", length) +
+						getTag("#info-lg.col-lg-2 visible-lg", info) +
+						getTag("#pagination.col-lg-5 col-sm-8", pagination)) +
+				getTag("row hidden-lg",
+						getTag("#info-non-lg", info) +
+						getTag("visible-xs", length))),
+
+		"lengthMenu": [
+				[10, 25, 50, 100, -1], 
+				[10, 25, 50, 100, "All"]],
+				
+		"order" : [ 
+				[ 0, 'asc' ], 
+				[ 1, 'asc' ], 
+				[ 2, 'asc' ], 
+				[ 3, 'asc' ]],
+				
+		"scrollX": true,
 		searching : false/*,
 		"serverSide": true*/
-	});
-});
+	});	
+}
+
+function getTag(clsId, content) {
+	return "<'" + clsId + "'" + content + ">";
+}
