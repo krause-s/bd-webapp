9b1127c7194736d48bdf1081de35d2c49c7241bd
Peter Seipel <pseipel1@uni-koeln.de>
2018-03-10 18:00:30 +0100

    user management wip

--- webapp/src/main/java/de/uni_koeln/dh/lyra/controller/IndexController.java
+++ webapp/src/main/java/de/uni_koeln/dh/lyra/controller/IndexController.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,89 +25,97 @@ import de.uni_koeln.dh.lyra.services.UserService;
 @Controller
 public class IndexController {
 
-		@Autowired
-		CorpusService corpusService;
-		
-		@Autowired
-		UserService userService;
+	@Autowired
+	CorpusService corpusService;
+
+	@Autowired
+	UserService userService;
 
 	@RequestMapping(value = { "", "/" })
 	public String index(Model model) {
 		System.out.println("NO index -> overlay");
-		
-		model.addAttribute("upload", true);
+		boolean showUpload = true;
+		if (userService.loggedIn()) {
+			showUpload = false;
+		}
+		model.addAttribute("upload", showUpload);
 		return "index";
 	}
-	
+
 	@PostMapping(value = "/upload")
 	public String upload(Model model) {
 		System.out.println("UPLOAD");
-		
+
 		// TODO file upload (idle)
-		List<Place> placesToEvaluate = 
-				corpusService.init(CorpusService.dataPath);
-//				Arrays.asList(new Place[] { 
-//						new Place(0, 0, Arrays.asList(new PopUp[] { new PopUp("cambridge"), new PopUp("cambridge") }), false), 
-//						new Place(1, 1, Arrays.asList(new PopUp[] { new PopUp("new york"), new PopUp("new york city"), new PopUp("new york") }), false),
-//						new Place(2, 2, Arrays.asList(new PopUp[] { new PopUp("koeln") }), false),});
-		
+		List<Place> placesToEvaluate = corpusService.init(CorpusService.dataPath);
+		// Arrays.asList(new Place[] {
+		// new Place(0, 0, Arrays.asList(new PopUp[] { new PopUp("cambridge"),
+		// new PopUp("cambridge") }), false),
+		// new Place(1, 1, Arrays.asList(new PopUp[] { new PopUp("new york"),
+		// new PopUp("new york city"), new PopUp("new york") }), false),
+		// new Place(2, 2, Arrays.asList(new PopUp[] { new PopUp("koeln") }),
+		// false),});
+
 		if (placesToEvaluate != null) {
 			map = new HashMap<Place, Set<String>>();
-			
+
 			for (Place place : placesToEvaluate) {
 				Set<String> set = new TreeSet<String>();
-				
-				for (PopUp popup : place.getPopUps()) 
+
+				for (PopUp popup : place.getPopUps())
 					set.add(popup.getPlaceName());
-				
+
 				map.put(place, set);
 			}
-			
+
 			model.addAttribute("places", map);
 		} else {
 			// TODO no places -> no evaluation
 		}
-		
+
 		return "index";
 	}
-	
-		Map<Place, Set<String>> map;
-	
+
+	Map<Place, Set<String>> map;
+
 	@PostMapping(value = "/evaluation")
-	public String evaluation(@RequestParam(value = "placeName", required = false) List<String> placeNames, Model model) {
+	public String evaluation(@RequestParam(value = "placeName", required = false) List<String> placeNames,
+			Model model) {
 		System.out.println("EVALUATION");
 
 		// TODO placeNames == null
 
-		Iterator<Map.Entry<Place,Set<String>>> iter = map.entrySet().iterator();
-		
+		Iterator<Map.Entry<Place, Set<String>>> iter = map.entrySet().iterator();
+
 		while (iter.hasNext()) {
 			Set<String> set = iter.next().getValue();
-			
+
 			set.removeAll(placeNames);
-		    if (set.isEmpty()) iter.remove();
+			if (set.isEmpty())
+				iter.remove();
 		}
-		
+
 		corpusService.init2(map);
 		userService.generateNewID();
 		corpusService.serializeCorpus(userService.getUserID());
 		userService.init();
-		
+
 		// TODO idle, setId, model (stock)
-		
+
 		return "index";
 	}
-	
-	@RequestMapping(value =  "/{id}")
-	public String index(@PathVariable("id") String id/*, Model model*/) {
-		System.out.println("index " + id + " -> NO overlay");
-		corpusService.readExistingCorpus(id);
-		userService.setUserID(id);
-		
-//		userService.setUserID(id);	
-		return "index";
+
+	@RequestMapping(value = "/{id}")
+	public String index(@PathVariable("id") String id , Model model) {
+		if (corpusService.idExitst(id)) {
+			corpusService.readExistingCorpus(id);
+			userService.setUserID(id);
+		}else{
+			userService.setUserID(null);
+		}
+		return "redirect:/";
 	}
-	
+
 	@RequestMapping(value = "/about")
 	public String about() {
 		return "about";
