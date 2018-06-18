bca1b4fde2d84eb649db34cd22bce7671182067c
Peter Seipel <pseipel1@uni-koeln.de>
2018-06-18 20:00:58 +0200

    small fix

--- webapp/src/main/java/de/uni_koeln/dh/lyra/controller/AnalysisController.java
+++ webapp/src/main/java/de/uni_koeln/dh/lyra/controller/AnalysisController.java
@@ -2,7 +2,9 @@ package de.uni_koeln.dh.lyra.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
